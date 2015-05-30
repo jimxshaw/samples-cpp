d7e28eadaf1043805f6aa0a7adb5fdd512b8c2e1
Jim Shaw <jimxshaw@gmail.com>
2015-05-29 22:36:56 -0500

    added functions

--- toupper-tolower/source.cpp
+++ toupper-tolower/source.cpp
@@ -3,8 +3,35 @@
 
 using namespace std;
 
-int main() {
+void ToUpperCase(char s[]) {
+	for (int i = 0; i < strlen(s); i++) {
+		s[i] = toupper(s[i]);
+	}
+}
 
+void ToLowerCase(char s[]) {
+	for (int i = 0; i < strlen(s); i++) {
+		s[i] = tolower(s[i]);
+	}
+}
+
+int main() {
+	char fn[100];
+
+	cout << "Enter an input file name: ";
+	cin >> fn;
+	ifstream infile(fn);
+
+	if (infile) {
+		while (infile.getline(fn, 100)) {
+			ToUpperCase(fn);
+			cout << fn << endl;
+		}
+		infile.close();
+	}
+	else {
+		cout << "Invalid file" << endl;
+	}
 
 
 
